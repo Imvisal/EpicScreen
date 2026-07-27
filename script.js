93a1d07f33013531c9acc70b02d1bea8a4901b2d
Imvisal <81892619+Imvisal@users.noreply.github.com>
2026-07-28 00:42:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
+const API_KEY = "cbbd63fd";
 
+const searchBtn = document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+const movies = document.getElementById("movies");
+
+searchBtn.addEventListener("click", searchMovie);
+
+searchInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        searchMovie();
+    }
+});
+
+async function searchMovie() {
+
+    const movie = searchInput.value.trim();
+
+    if (movie === "") {
+        alert("Please enter a movie name");
+        return;
+    }
+
+    movies.innerHTML = "<h2>Loading...</h2>";
+
+    try {
+
+        const response = await fetch(
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movie}`
+        );
+
+        const data = await response.json();
+
+        if (data.Response === "False") {
+            movies.innerHTML = "<h2>Movie Not Found</h2>";
+            return;
+        }
+
+        displayMovies(data.Search);
+
+    } catch {
+
+        movies.innerHTML = "<h2>Something went wrong!</h2>";
+
+    }
+
+}
+
+function displayMovies(list) {
+
+    movies.innerHTML = "";
+
+    list.forEach(movie => {
+
+        movies.innerHTML += `
+
+        <div class="movie">
+
+            <img src="${
+                movie.Poster !== "N/A"
+                ? movie.Poster
+                : "https://placehold.co/300x450?text=No+Image"
+            }">
+
+            <div class="movie-info">
+
+                <h3>${movie.Title}</h3>
+
+                <p>📅 ${movie.Year}</p>
+
+                <button class="details-btn">
+                    View Details
+                </button>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+}
